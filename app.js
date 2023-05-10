367df50097279e2537d516397b290ad9fc86ff25
CorvusCodex <56726555+CorvusCodex@users.noreply.github.com>
2023-05-10 11:54:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 
 process.title = "Solana Bruteforce";
 
+//Creaded by: Corvus Codex
+//Github: https://github.com/CorvusCodex/
+//Licence : MIT License
+
+//Support my work:
+//BTC: bc1q7wth254atug2p4v9j3krk9kauc0ehys2u8tgg3
+//ETH & BNB: 0x68B6D33Ad1A3e0aFaDA60d6ADf8594601BE492F0
+//Buy me a coffee: https://www.buymeacoffee.com/CorvusCodex
+
 // Importing required modules
 const { Keypair } = require('@solana/web3.js');
 const fs = require('fs');
