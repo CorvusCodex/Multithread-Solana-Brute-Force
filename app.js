11fc479f1db538239e0a6cda33ff6c5e52cd92de
CorvusCodex <56726555+CorvusCodex@users.noreply.github.com>
2025-01-19 17:48:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ process.title = "Multithread Solana Brute-Force by Corvus Codex";
 //Github: https://github.com/CorvusCodex/
 //Licence : MIT License
 
-//Support my work:
+//Support my work/ Donate:
 //SOL: FsX3CsTFkRjzne2KiD8gjw3PEW2bYqezKfydAP55BVj7
 //BTC: bc1q7wth254atug2p4v9j3krk9kauc0ehys2u8tgg3
 //POLYGON, ETH & BNB: 0x68B6D33Ad1A3e0aFaDA60d6ADf8594601BE492F0
