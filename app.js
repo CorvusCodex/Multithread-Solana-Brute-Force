064faf2c1cb3277ba3e3201ee73e88548287f59b
CorvusCodex <56726555+CorvusCodex@users.noreply.github.com>
2025-02-27 20:59:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ process.title = "Multithread Solana Brute-Force by Corvus Codex";
 //POL, ETH & BNB...: 0x68B6D33Ad1A3e0aFaDA60d6ADf8594601BE492F0
 //Buy me a coffee: https://www.buymeacoffee.com/CorvusCodex
 //https://ko-fi.com/s/547b50850e
+//===============================================================
 
 
 const { Keypair } = require('@solana/web3.js');
